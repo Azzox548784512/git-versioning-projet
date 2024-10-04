8166579e4be00f82a8a8f7bccf4343d983a43e21
ilian touati <iliantouati689@gmail.com>
2024-10-04 08:39:00 -1000

    commencement du dev du jeu j'ai insérer une fonction de création de liste des 100 chiffres et initié les classes de test unitaire

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from random import *
 from math import *
+import unittest
+
+class TestEntryUser :
+    pass
 
 
-print('commencement du projet')
+class TestCompAnswer : 
+    def test_reponse_trouvee(self):
+        pass
+
+def create_liste():
+    liste_1à100 = []
+    for i in range(1,101):
+        liste_1à100.append(i)
+    print(liste_1à100)
+    random_number = randint(1,100)
+
+
+
+
