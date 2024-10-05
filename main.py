0a9116362ddea74a0bc5b7eaebfcae74a6e1eb66
ilian touati <iliantouati689@gmail.com>
2024-10-05 02:37:31 -1000

    second commit ajout de la recherche dichotomique et quelques autres fonctions

--- main.py
+++ main.py
@@ -6,16 +6,47 @@ class TestEntryUser :
     pass
 
 
-class TestCompAnswer : 
+class TestEntryComp : 
     def test_reponse_trouvee(self):
         pass
 
-def create_liste():
+def liste_Chiffre():
     liste_1à100 = []
     for i in range(1,101):
         liste_1à100.append(i)
-    print(liste_1à100)
-    random_number = randint(1,100)
+    return liste_1à100
+
+
+
+def recherche_dichotomique(tab): # pour l'ordinateur 
+    elemT = False
+    debut = 0 # le chiffre 1
+    fin = len(tab) # le chiffre 100
+    while elemT != True : 
+        réponse = str(input('plus grand, plus petit ?'))
+        millieu = len(tab)//2 # le chiffre 50
+        if  réponse == 'plus petit' : 
+            fin = millieu
+            print(fin)
+
+        elif réponse == 'plus grand' :
+            debut = millieu 
+            print(debut)
+
+        elif debut == fin : 
+            print('trouvé le chiffre était : ',millieu)
+            elemT = True
+    return True
+
+def chiffre_aléatoire(): # pour l'utilisateur 
+    return randint(1,101)
+
+print('chiffre aléatoire',chiffre_aléatoire())
+recherche_dichotomique(liste_Chiffre()) 
+
+
+
+
 
 
 
